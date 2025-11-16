34891c96db29b59a2984127f875e6861dd5282b0
Hypnoscat <maksymkirianov697@gmail.com>
2025-11-16 19:37:37 +0100

    add icon app , and description

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,432 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stop Watch!!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        brightness: Brightness.light,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String timerString = "00:00.00";
+  bool isTimerActive = false;
+  bool nextState = false;
+  bool nextStateButtns = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  late Stream<int> tickStream;
+  late Stream<int> tenthofsecondStream;
+  late StreamSubscription<int> secondStreamSubscription;
+
+  void startTick() {
+    tickStream = Stream.periodic(Duration(milliseconds: 10), (x) => x);
+
+    tenthofsecondStream = tickStream.map((tick) => tick);
+
+    secondStreamSubscription = tenthofsecondStream.listen((tick) {
+      if (!isPause) {
+        //print('Second: $tick');
+        tenthofsecond = tick % 101;
+        updateTimerUI(tenthofsecond);
+      }
     });
+  }
+
+  int minutes = 0;
+  int seconds = 0;
+  int tenthofsecond = 0;
+
+  void updateTimerUI(int tenthofsecond) {
+    setState(() {
+      String tempTS = "00";
+      String tempS = "00";
+      String tempM = "00";
+
+      
+      if (tenthofsecond == 100) {
+        seconds++;
+        tenthofsecond = 0; 
+      }
+
+      if (tenthofsecond < 10) {
+        tempTS = "0" + tenthofsecond.toString();
+      } else {
+        tempTS = tenthofsecond.toString();
+      }
+
+      if (seconds == 60) {
+        minutes++;
+        seconds = 0;
+      }
+     
+      if (seconds < 10) {
+        tempS = "0" + seconds.toString();
+      } else {
+        tempS = seconds.toString();
+      }
+
+      if (minutes == 60) {
+        minutes = 0;
+      }
+
+      if (minutes < 10) {
+        tempM = "0" + minutes.toString();
+      } else {
+        tempM = minutes.toString();
+      }
+      // Оновлюємо рядок таймера
+      timerString = tempM + ":" + tempS + "." + tempTS;
+    });
+  }
+
+  void stopTimer() {
+    secondStreamSubscription.cancel();
+    minutes = 0;
+    seconds = 0;
+    tenthofsecond = 0;
+  }
+
+  bool isPause = false;
+  void pause() {
+    setState(() {
+      if (isTimerActive) {
+        isPause = !isPause;
+        if (isPause) {
+          secondStreamSubscription.pause();
+        } else {
+          secondStreamSubscription.resume();
+        }
+      }
+    });
+  }
+
+  List<Widget> textWidgets = [];
+  List<String> listtime = [];
+  void addTime(String time) {
+    setState(() {
+     
+      int index = textWidgets.length + 1;
+      String formattedIndex = index < 10 ? "0$index" : index.toString();
+
+      textWidgets.insert(
+          0,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                formattedIndex,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "+" + bestTime(time),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
+              Text(
+                time,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ],
+          )); 
+          listtime.add(time);
+    });
+  }
+
+  String bestTime(String time) {
+    if (listtime.isEmpty) {
+      return time;
+    }
+    print(time);
+    List<String> newParts = time.split(':');
+    int newMinutes = int.parse(newParts[0]);
+    List<String> secParts = newParts[1].split('.');
+    int newSeconds = int.parse(secParts[0]);
+    int newTenths = int.parse(secParts[1]);
+
+    String lastTime = listtime.last;
+
+    print(lastTime);
+    List<String> oldParts = lastTime.split(':');
+    int oldMinutes = int.parse(oldParts[0]);
+    List<String> oldSecParts = oldParts[1].split('.');
+    int oldSeconds = int.parse(oldSecParts[0]);
+    int oldTenths = int.parse(oldSecParts[1]);
+
+    int newTotal = (newMinutes * 60 * 100) + (newSeconds * 100) + newTenths;
+    int oldTotal = (oldMinutes * 60 * 100) + (oldSeconds * 100) + oldTenths;
+
+    int diff = newTotal - oldTotal;
+
+    int diffMinutes = diff ~/ 6000;
+    int diffSeconds = (diff % 6000) ~/ 100;
+    int diffTenths = diff % 100;
+
+    String tempM = diffMinutes < 10 ? "0$diffMinutes" : diffMinutes.toString();
+    String tempS = diffSeconds < 10 ? "0$diffSeconds" : diffSeconds.toString();
+    String tempTS = diffTenths < 10 ? "0$diffTenths" : diffTenths.toString();
+    print("| $diffMinutes : $diffSeconds . $diffTenths |");
+
+    return "$tempM:$tempS.$tempTS";
+  }
+
+  Widget body() {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: textWidgets.isEmpty
+                ? EdgeInsets.symmetric(horizontal: 100, vertical: 100)
+                : EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue, width: 2),
+              shape: textWidgets.isEmpty ? BoxShape.circle : BoxShape.rectangle,
+              borderRadius:
+                  textWidgets.isEmpty ? null : BorderRadius.circular(20),
+            ),
+            child: Text(
+              timerString,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          if (textWidgets.isNotEmpty) slider()
+        ],
+      ),
+    );
+  }
+
+  Widget slider() {
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Column(
+          children: textWidgets,
+        ),
+      ),
+    );
+  }
+
+  Widget buttonAction() {
+    if (!nextState) {
+      return TextButton(
+        style: TextButton.styleFrom(
+          side: BorderSide(color: Colors.blue, width: 2),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        ),
+        onPressed: () => setState(() {
+          startTick();
+          nextState = true;
+          isTimerActive = true;
+          print("next $nextState");
+        }),
+        child: Text(
+          "start",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+    } else {
+      if (nextStateButtns) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                side: BorderSide(color: Colors.blue, width: 2),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () => setState(() {
+                stopTimer();
+                textWidgets.clear();
+                isTimerActive = false;
+                isPause = false;
+                nextState = false;
+                nextStateButtns = false;
+                timerString = "00:00.00";
+                print("next $nextState");
+              }),
+              child: Text(
+                "stop",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                side: BorderSide(color: Colors.blue, width: 2),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () => setState(() {
+                pause();
+                nextStateButtns = false;
+              }),
+              child: Text(
+                "continue",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      } else {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              style: IconButton.styleFrom(
+                side: BorderSide(color: Colors.blue, width: 2),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () {
+                addTime(timerString);
+              },
+              child: Text(
+                "point",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                side: BorderSide(color: Colors.blue, width: 2),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () => setState(() {
+                pause();
+                nextStateButtns = true;
+              }),
+              child: Text(
+                "pause",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(
+              height: 60,
+            ),
+            body(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buttonAction(),
+              ],
+            ),
+            SizedBox(
+              height: 20,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Color.fromARGB(255, 56, 56, 56),
+              width: 1,
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.timer,
+              color: Colors.white,
+              size: 40,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
